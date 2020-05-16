1f4ade4af9e1e990bd634389b5fa8341a844829b
Martynenko <4lexm4rtin@gmail.com>
2020-05-17 00:22:37 +0300

    fixManyBags_addedChangesPlayers_chengeCalculationNumberOfBalls

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-///////////////////////// MAX SCORE PLAYER
+
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
@@ -11,10 +11,8 @@ const minsize = document.getElementById('minsize')
 const minsizevalue = document.getElementById('minsizevalue')
 const speed = document.getElementById('speed')
 const speedvalue = document.getElementById('speedvalue')
-const maxnumber = document.getElementById('maxnumber')
-const maxnumbervalue = document.getElementById('maxnumbervalue')
-const minnumber = document.getElementById('minnumber')
-const minnumbervalue = document.getElementById('minnumbervalue')
+const ballsnumber = document.getElementById('ballsnumber')
+const ballsnumbervalue = document.getElementById('ballsnumbervalue')
 
 const scorelistname = document.getElementById('scorelistname')
 const scorelistscore = document.getElementById('scorelistscore')
@@ -24,44 +22,59 @@ const defaultbtn = document.getElementById('default')
 const changeplayer = document.getElementById('changeplayer')
 const newplayer = document.getElementById('newplayer')
 
+const listchangename = document.getElementById('listchangename')
 
-var maxSizeBall;
-var minSizeBall;
-var speedBall;
-var maxHowManyBalls;
-var howManyBalls;
+
+var maxHowManyBalls = 50;
+var howManyBalls = 8;
+var tempQuantiy;
+var checkRealyChange = 0;
+var distance;
+var distanceClick;
+var mouseX;
+var mouseY;
+var clickX;
+var clickY;
+var border = 65;
+var gameScore = 0;
+var gameStop = 0;
+var gameStart;
+var secGameStart = 0;
+var inputscorelist;
 
 let gameDataArray = {
   maxSizeBall: 14,
   minSizeBall: 8,
   speedBall: 5,
-  maxHowManyBalls: 50,
-  howManyBalls: 8,
+  quantityBalls: 0,
   gameMaxScore: 0,
   nameplayers: {
     lastplayer: undefined,
   }
 };
 
+
 if (localStorage.getItem('gameBalls')) {
   const dataGame = JSON.parse(localStorage.getItem('gameBalls'));
   for(let key in dataGame.nameplayers) {
-    // if (key != undefined && dataGame.nameplayers[key] != 0) {
-      if (key != "lastplayer") {
-        const liname = document.createElement('li');
-        liname.textContent = key;
-        scorelistname.appendChild(liname);
-        const liscore = document.createElement('li');
-        liscore.textContent = dataGame.nameplayers[key];
-        scorelistscore.appendChild(liscore);
-      }
-      gameDataArray.nameplayers[key] = dataGame.nameplayers[key];
-    // }
+    if (key != "lastplayer") {
+      const liname = document.createElement('li');
+      liname.textContent = key;
+      scorelistname.appendChild(liname);
+      const liscore = document.createElement('li');
+      liscore.textContent = dataGame.nameplayers[key];
+      scorelistscore.appendChild(liscore);
+      const lichangename = document.createElement('li');
+      lichangename.className = ('li-change-name');
+      lichangename.textContent = key;
+      listchangename.appendChild(lichangename);
+    }
+    gameDataArray.nameplayers[key] = dataGame.nameplayers[key];
   }
   for(let key in dataGame) {
     if(key != "nameplayers") {
       if(dataGame[key] != gameDataArray[key]) {
-        gameDataArray[key] = dataGame[key];
+        gameDataArray[key] = Number(dataGame[key]);
       } 
     }
   }
@@ -74,87 +87,49 @@ minsizevalue.textContent = ` : ${gameDataArray.minSizeBall}`;
 minsize.setAttribute('value', gameDataArray.minSizeBall);
 speedvalue.textContent = ` : ${gameDataArray.speedBall}`;
 speed.setAttribute('value', gameDataArray.speedBall);
-maxnumbervalue.textContent = ` : ${gameDataArray.maxHowManyBalls}`;
-maxnumber.setAttribute('value', gameDataArray.maxHowManyBalls);
-minnumbervalue.textContent = ` : ${gameDataArray.howManyBalls}`;
-minnumber.setAttribute('value', gameDataArray.howManyBalls);
+ballsnumbervalue.textContent = ` : ${gameDataArray.quantityBalls}`;
+ballsnumber.setAttribute('value', gameDataArray.quantityBalls);
+
+
+
 
 maxsize.addEventListener('change', function(e) {
   maxsizevalue.textContent = ` : ${e.target.value}`;
-  gameDataArray.maxSizeBall = e.target.value;
+  gameDataArray.maxSizeBall = Number(e.target.value);
+  checkRealyChange = 1;
   localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
 })
 minsize.addEventListener('change', function(e) {
   minsizevalue.textContent = ` : ${e.target.value}`;
-  gameDataArray.minSizeBall = e.target.value;
+  gameDataArray.minSizeBall = Number(e.target.value);
+  checkRealyChange = 1;
   localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
 })
 speed.addEventListener('change', function(e) {
   speedvalue.textContent = ` : ${e.target.value}`;
-  gameDataArray.speedBall = e.target.value;
+  gameDataArray.speedBall = Number(e.target.value);
+  checkRealyChange = 1;
   localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
 })
-maxnumber.addEventListener('change', function(e) {
-  maxnumbervalue.textContent = ` : ${e.target.value}`;
-  gameDataArray.maxHowManyBalls = e.target.value;
-  localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
-})
-minnumber.addEventListener('change', function(e) {
-  minnumbervalue.textContent = ` : ${e.target.value}`;
-  gameDataArray.howManyBalls = e.target.value;
+ballsnumber.addEventListener('change', function(e) {
+  ballsnumbervalue.textContent = ` : ${e.target.value}`;
+  gameDataArray.quantityBalls = Number(e.target.value);
   localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
 })
 
 
 
-var pixels = (width / 500) * (height / 300);
-var quantityBalls = Math.floor(pixels * gameDataArray.howManyBalls);
-if (quantityBalls > gameDataArray.maxHowManyBalls) {
-  gameDataArray.maxSizeBall = ((quantityBalls - gameDataArray.maxHowManyBalls) / 100 + 1) * gameDataArray.maxSizeBall;
-  gameDataArray.minSizeBall = ((quantityBalls - gameDataArray.maxHowManyBalls) / 100 + 1) * gameDataArray.minSizeBall;
-  quantityBalls = gameDataArray.maxHowManyBalls;
-}
-
-
-var distance;
-var distanceClick;
-var mouseX;
-var mouseY;
-var clickX;
-var clickY;
-var border = 65;
-var gameScore = 0;
-// var gameMaxScore = 0;
-var gameStop = 0;
-var gameStart;
-var inputscorelist;
-
-$("#cog").click(function(event){
-  $(".settings,.fa-cog").toggleClass("active");
-  // gameStop = 1;
-});
-$("#top").click(function(event){
-  $(".score-list,.fa-top").toggleClass("active");
-  // gameStop = 1;
-});
 defaultbtn.addEventListener('click', function(e){
-  localStorage.removeItem('gameBalls');
-  // maxSizeBall: 14,
-  // minSizeBall: 8,
-  // speedBall: 5,
-  // maxHowManyBalls: 50,
-  // howManyBalls: 8,
-  // gameMaxScore: 0,
+  localStorage.removeItem('gameBalls');  
 })
 changeplayer.addEventListener('click', function(e){
-  $(".fa-play,.nameplayer,.name-label").toggleClass("hidden");
+  $(".list-change-name").toggleClass("active");
 })
 newplayer.addEventListener('click', function(e){
   $(".fa-play,.nameplayer,.name-label").toggleClass("hidden");
   gameDataArray.nameplayers.lastplayer = undefined;
   localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
 })
-
 if(gameDataArray.nameplayers.lastplayer != undefined) {
   $(".fa-play,.nameplayer").toggleClass("hidden");
   inputscorelist = gameDataArray.nameplayers.lastplayer;
@@ -162,9 +137,78 @@ if(gameDataArray.nameplayers.lastplayer != undefined) {
   namelabel.className = ('name-label');
   namelabel.textContent = inputscorelist;
   body.appendChild(namelabel);
+  secGameStart++;
   setTimeout(function(){ 
     gameStart = 1;
-  }, 2500);
+  }, 3000);
+}
+$(".li-change-name").click(function(event){
+  inputscorelist = this.innerText;
+  gameDataArray.nameplayers.lastplayer = inputscorelist;
+  localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
+  setTimeout(function(){ 
+    location.reload();
+  }, 1000);
+})
+$("#cog").click(function(event){
+  $(".settings,.fa-cog").toggleClass("active");
+});
+$("#top").click(function(event){
+  $(".score-list,.fa-top").toggleClass("active");
+});
+$("#play").click(function(event){
+  inputscorelist = nameplayer.value;
+  var checkValidatename = checkValidname(inputscorelist);
+  if (inputscorelist.length > 2 && inputscorelist.length < 13 && checkValidatename === 1) {
+    gameDataArray.nameplayers.lastplayer = inputscorelist;
+    $(".fa-play").toggleClass("active");
+    $(".fa-play,.nameplayer").toggleClass("hidden");
+    if(gameDataArray.nameplayers[inputscorelist] === undefined){
+      gameDataArray.nameplayers[inputscorelist]  = gameScore;
+    } else {
+      gameDataArray.nameplayers[inputscorelist] = gameDataArray.nameplayers[inputscorelist];
+    }
+    localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
+    const namelabel = document.createElement('p');
+    namelabel.className = ('name-label');
+    namelabel.textContent = inputscorelist;
+    body.appendChild(namelabel);
+    secGameStart++;
+    setTimeout(function(){ 
+      gameStart = 1;
+    }, 3000);
+  }
+});
+
+function quantityOnPixels() {
+  var pixels = (width / 500) * (height / 300);
+  gameDataArray.quantityBalls = Math.floor(pixels * howManyBalls);
+  if (gameDataArray.quantityBalls > maxHowManyBalls) {
+    gameDataArray.maxSizeBall = Math.round(((gameDataArray.quantityBalls - maxHowManyBalls) / 100 + 1) * gameDataArray.maxSizeBall);
+    gameDataArray.minSizeBall = Math.round(((gameDataArray.quantityBalls - maxHowManyBalls) / 100 + 1) * gameDataArray.minSizeBall);
+    gameDataArray.quantityBalls = maxHowManyBalls;
+  }
+  if (localStorage.getItem('gameBalls')) {
+    const dataGame = JSON.parse(localStorage.getItem('gameBalls'));
+    if (dataGame.quantityBalls != gameDataArray.quantityBalls) {
+      gameDataArray.quantityBalls = Number(dataGame.quantityBalls);
+      gameDataArray.maxSizeBall = Number(dataGame.maxSizeBall);
+      gameDataArray.minSizeBall = Number(dataGame.minSizeBall);
+    }  
+  }  
+}
+function realyChangeNull() {
+  if (checkRealyChange === 1) {
+    tempQuantiy = gameDataArray.quantityBalls;
+    gameDataArray.quantityBalls = 0;
+    checkRealyChange = 2;
+  }
+}
+function realyChangeNorm() {
+  if (checkRealyChange === 2) {
+    gameDataArray.quantityBalls = tempQuantiy;
+    checkRealyChange = 0;
+  }
 }
 function checkValidname(name) {
   var characterValid = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
@@ -184,27 +228,6 @@ function checkValidname(name) {
     return 0;
   }
 }
-$("#play").click(function(event){
-  inputscorelist = nameplayer.value;
-  var checkValidatename = checkValidname(inputscorelist);
-  if (inputscorelist.length > 2 && inputscorelist.length < 13 && checkValidatename === 1) {
-    gameDataArray.nameplayers.lastplayer = inputscorelist;
-    $(".fa-play").toggleClass("active");
-    $(".fa-play,.nameplayer").toggleClass("hidden");
-    if(gameDataArray.nameplayers[inputscorelist] === undefined){
-      gameDataArray.nameplayers[inputscorelist]  = gameScore;
-    } else {
-      gameDataArray.nameplayers[inputscorelist] = gameDataArray.nameplayers[inputscorelist];
-    }
-    localStorage.setItem('gameBalls', JSON.stringify(gameDataArray));
-    const namelabel = document.createElement('p');
-    namelabel.className = ('name-label');
-    namelabel.textContent = inputscorelist;
-    body.appendChild(namelabel);
-    gameStart = 1;
-  }
-});
-
 function getMouse() {
   if (gameStart === 1) {
     $("#canvas").mousemove(function(event){
@@ -221,7 +244,6 @@ function getMouseClick() {
     });
   }
 };
-
 function random(min, max) {
   var num = 0;
   while (num === 0) {
@@ -285,6 +307,39 @@ function checkMouseOnGamePlace() {
     gameScore = 0;
   }
 };
+function drawGameStart() {
+  if (secGameStart >= 1) {  
+    if (secGameStart < 51) {
+      ctx.font = "Bold 90px serif";
+      ctx.lineWidth = 2;
+      ctx.fillStyle = "White";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText("1", width / 2, height / 2);
+      ctx.strokeStyle = "Gray";
+      ctx.strokeText("1", width / 2, height / 2);
+    } else if (secGameStart < 101) {
+      ctx.font = "Bold 110px serif";
+      ctx.lineWidth = 2;
+      ctx.fillStyle = "White";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText("2", width / 2, height / 2);
+      ctx.strokeStyle = "Gray";
+      ctx.strokeText("2", width / 2, height / 2);
+    } else if (secGameStart < 151) {
+      ctx.font = "Bold 130px serif";
+      ctx.lineWidth = 2;
+      ctx.fillStyle = "White";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText("3", width / 2, height / 2);
+      ctx.strokeStyle = "Gray";
+      ctx.strokeText("3", width / 2, height / 2);
+    }
+    secGameStart++
+  }
+}
 function gameOver() {
   if (gameStop >= 1) {
     if (gameScore > gameDataArray.gameMaxScore) {
@@ -299,6 +354,7 @@ function gameOver() {
     ctx.fillText("Game Over", width / 2, height / 2);
     ctx.strokeStyle = "Gray";
     ctx.strokeText("Game Over", width / 2, height / 2);
+    gameStart = 0;
     gameStop++;
     var maxPlayerScore = gameDataArray.nameplayers[inputscorelist];
     if (maxPlayerScore < gameScore) {
@@ -309,7 +365,7 @@ function gameOver() {
     localStorage.setItem('gameBalls', JSON.stringify(gameDataArray))
     setTimeout(function(){ 
       clearInterval(drawGameBalls);
-    }, 2500);
+    }, 3000);
   }
 };
 
@@ -329,7 +385,7 @@ Ball.prototype.move = function() {
     this.y += this.ySpeed;
 };
 Ball.prototype.moveStop = function() {
-  if (gameStop === 1 || gameStop === 20 || gameStop === 40 || gameStop === 60 || gameStop === 80 || gameStop === 100) {
+  if (gameStop === 1 || gameStop === 20 || gameStop === 40 || gameStop === 60 || gameStop === 80 || gameStop === 100 || gameStop === 120) {
     if(this.xSpeed === 0){this.xSpeed = 0;}
     if(this.xSpeed > 0){this.xSpeed--;}
     if(this.xSpeed < 0){this.xSpeed++;}
@@ -365,7 +421,9 @@ function BallScore(color, size) {
   this.size = size;
 };
 BallScore.prototype.draw = function () {
-  circle(this.x, this.y, this.size, true, this.color);
+  if (gameStart === 1) {
+    circle(this.x, this.y, this.size, true, this.color);
+  }
 };
 BallScore.prototype.checkClick = function() {
   distanceClick = Math.sqrt(((clickX - this.x) * (clickX - this.x)) + ((clickY - this.y) * (clickY - this.y)));
@@ -373,6 +431,7 @@ BallScore.prototype.checkClick = function() {
     if(this.color === "Green") {
       this.x = random(0 + gameDataArray.maxSizeBall * 2, width - gameDataArray.maxSizeBall * 2);
       this.y = random(0 + gameDataArray.maxSizeBall * 2 + border, height - gameDataArray.maxSizeBall * 2);
+      this.size = gameDataArray.maxSizeBall;
       gameScore++;
     } else if(this.color === "Yellow") {
       this.size = 0;
@@ -390,22 +449,31 @@ var ballYellow = new BallScore("Yellow", 0);
 var ballBlue  = new BallScore("Blue", 0);
 
 setInterval(function() {
-  ballYellow = new BallScore("Yellow", ((gameDataArray.maxSizeBall - gameDataArray.minSizeBall) / 2 + gameDataArray.minSizeBall));
+  if (gameStart === 1) {
+    ballYellow = new BallScore("Yellow", ((gameDataArray.maxSizeBall - gameDataArray.minSizeBall) / 2 + gameDataArray.minSizeBall));
+  }
 }, random(6000,16000));
 setInterval(function() {
-  ballBlue = new BallScore("Blue", gameDataArray.minSizeBall);
+  if (gameStart === 1) {
+    ballBlue = new BallScore("Blue", gameDataArray.minSizeBall);
+  }
 }, random(16000,35000));
 setInterval(checkMouseOnGamePlace, 100);
 
 
-
+quantityOnPixels();
 var balls = [];
 var drawGameBalls = setInterval(function() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.35)';
   ctx.fillRect(0,0,width,height);
-  while (balls.length < quantityBalls) {
+  realyChangeNorm();
+  while (balls.length < gameDataArray.quantityBalls) {
     var ball = new Ball("White");
     balls.push(ball);
+  }
+  realyChangeNull();
+  while (balls.length > gameDataArray.quantityBalls) {
+    balls.pop(ball);
   }
   drawBorder();
   getMouse();
@@ -424,6 +492,7 @@ var drawGameBalls = setInterval(function() {
     item.checkDistance();
     item.moveStop();
   })
+  drawGameStart();
   drawMaxScore();
   drawScore();
   gameOver();
